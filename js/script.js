e59d296a1f02cc16fb0fd504b04056a232086ba9
Genesis Mangunay <genesis.mangunay7@gmail.com>
2022-02-08 18:57:35 -0800

    Randomizing function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
+//Array of responses
+const responses = [
+    "As I see it, yes",
+	"It is certain",
+	"It is decidedly so",
+	"Most likely",
+	"Outlook good",
+	"Signs point to yes",
+	"Without a doubt",
+	"Yes",
+	"Yes - definitely",
+	"You may rely on it",
+    "Reply hazy, try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+    "Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+];
+//Function for finding random index in an array
+function getRandomResponse(arr){
 
+    // get random index value
+    const random = Math.floor(Math.random() * arr.length);
+
+    // get random response
+    const response = arr[random];
+
+    return response;
+}
+//Function for return random index of responses[]
+function results(){
+    const result = getRandomResponse(responses);
+    console.log(result); 
+}
