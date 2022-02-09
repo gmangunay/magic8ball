305b7c5cc5cb826cd8ba29ef2709f9fe2afb09d3
Genesis Mangunay <genesis.mangunay7@gmail.com>
2022-02-08 18:05:57 -0800

    update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// let input = document.getElementById("typeText");
+// input.addEventListener("keydown", function (execute) {
+//  if (execute.key === "Enter") {  
+//    validate(execute);
+//  }
+// });
+
+// function validate(execute) {
+// let text = execute.target.value;
+//  alert(text);
+// }
