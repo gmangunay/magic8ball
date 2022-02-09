796b6bc42c29286f0b54d0ccd81b5ce06121bbb2
Genesis Mangunay <genesis.mangunay7@gmail.com>
2022-02-08 18:23:44 -0800

    insert input field

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
 
-// let input = document.getElementById("typeText");
-// input.addEventListener("keydown", function (execute) {
-//  if (execute.key === "Enter") {  
-//    validate(execute);
-//  }
-// });
-
-// function validate(execute) {
-// let text = execute.target.value;
-//  alert(text);
-// }
